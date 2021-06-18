0f301b1da2199805a56b8139d764c4c5b58ad0ce
stimch <59399058+Stimch@users.noreply.github.com>
2021-06-18 18:52:08 +0000

    prev-version

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = discord.Client() #initialization
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(client)) #console
 
 
 @client.event
